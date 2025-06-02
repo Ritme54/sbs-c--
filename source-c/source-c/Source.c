66554f1f7ba32bc82317beced5574b999c9697b7
Ritme54 <tmdwns5456@naver.com>
2025-06-02 15:50:01 +0900

    2025.06.02(제귀함수)

--- source-c/source-c/Source.c
+++ source-c/source-c/Source.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 
-//void Stats(int health, const char* name)
-//{
-//
-//	printf("health : %d\n", health);
-//	printf("name : %s\n", name);
-//
-//
-//}
+void Stats(int health, const char* name)
+{
 
-//void Swap(int left, int right)
-//{
-//	int temporary = left;
-//	left = right;
-//	right = temporary;
-//
-//
-//}
-//
+	printf("health : %d\n", health);
+	printf("name : %s\n", name);
+
+
+}
+
+void Swap(int *left, int *right)
+{
+	int temporary = *left;
+	*left = *right;
+	*right = temporary;
+
+
+}
+
+
+inline void collision()
+{
+	
+	printf("You Die");
+
+}
+
+void Provide()
+{
+	printf("Provide\n");
+
+}
+
 void main() 
 {
 
@@ -30,6 +45,8 @@ void main()
 	// 
 	//함수는 프로그램이 실행 중일때 함수 호출 위치에서 해당 함수로 이동한 뒤,
 	//작업을 수행하고 다시 원래 위치로 돌아오는 구조
+	// 함수가 작동하면 함수를 찾아가서 위에서 아래로 실행한다.
+	// 
 		//process();
 
 
@@ -58,32 +75,17 @@ void main()
 
 	//int X = 10;
 	//int Y = 20;
-	//
-	//int x = &X;
-	//int y = &Y;
-	//
-	//printf("%p\n", &X);
-	//
-	//printf("%p\n", &Y);
-	//printf("%d\n", Y);
-	//임시변수
-	//
-	//  int Z = Y;
-	 // Y = X;
-	 // X = Z;
-	//
-
-	//Swap(&x, &y);
-	//
-	//printf("x의 값 : %p\n", x);
-	//printf("y의 값 : %p\n", y);
-	//
+	//Swap(&X, &Y);
+	//printf("X의 값 : %p\n", X);
+	//printf("Y의 값 : %p\n", Y);
+	
 
 #pragma endregion
 
 
 
 #pragma endregion
+
 
 #pragma region 레퍼런스
 
@@ -99,7 +101,100 @@ void main()
 #pragma endregion
 
 
+#pragma region 재귀 함수
+	//어떤 함수에서 자신을 다시 호출하여 작업을 수행하는 함수입니다.
+		
+	//Recursion(3);
+	
+	//return = 함수를 끝낸다.(종료)
+	
+	//void Recursion(int count)
+//{
+//
+//	if (count <=0)
+//	{
+//		return;
+//	}
+//
+//	printf("Recursion\n");
+//	Recursion(count - 1);
+//}
+
+
+
+#pragma endregion
+
+#pragma region 인라인 함수
+	//함수를 호출하는 대신 함수가 호출되는 위치마다 함수의 코드를 복사하여 전달하는 방식의 함수
+
+	//이벤트 기반 = on이란 전치사 많이 사용
+	// 오버해드(찾아보기)
+	//in line
+
+	
+	//collision();
+
+
+
+	//인라인 함수는 함수를 호출하는 과정이 없으므로 처리 속도가 빠르지만
+	//인라인 함수를 많이 사용하게 되면 함수의 코드가 복사되기 때문에 실행 파일의 크기가 커지게 된다.
+
+
+#pragma endregion
+
+#pragma region 가변 인수 목록
+	//고정되어 있는 매개 변수 외에 개수가 정해지지 않은 인수를 추가로 받을 수 있는 인수입니다.
+
+	//stdarg.h
+	//va_list 인수 목록
+	//va_start 시작 포인터 주소 가르키기
+	//->va_arg 값을 반환하는 함수
+	//va_end (안해도 큰 문제가 없을 수 있음) NULL로 초기화를 시켜주는 변수
+	
+	//void average(int count, ...)
+	// 
+	//va_list : 가변 인수의 메모리 주소를 저장하는 포인터 변수
+	//va_list : 변수
+	//va_start : 가변 인수를 가져올 수 있도록 포인터를 설정하는 함수
+	//va_start(포인터, count)
+	// va_arg : 가변 인수 포인터에서 특정 자료형의 크기만큼 값을 가져오는 함수
+	// va_arg(포인터, 특정한 자료형(int 또는 duble)
+	// va_end : 가변 인수가 끝났을 때 포인터를 NULL로 초기화하는 함수
+	// va_end(포인터)	
+
+
+#pragma endregion
+
+#pragma region 함수 포인터
+	//함수의 주소 값을 저장하고 가리킬 수 있는 포인터 변수입니다.
+	//자료형(*변수 이름)(매개변수, 매개변수)
+	//대신 호출하는 방식
+
+
+
+
+//포인터가 시작 주소를 가리킨다.
+
+//void Provide()
+//{
+//	printf("Probide\n")
+//
+//}
+
+//서로 커플링 시키지 않고 이벤트 버스를 만들어서 대신 호출하게 한다.
+
+void(*functionPointer)();
+
+functionPointer = Provide;
+
+functionPointer();
+
+	
+#pragma endregion
+
 
 
 
 }
+
+
--- source-c/구구단 숙제/레퍼.c
+++ source-c/구구단 숙제/레퍼.c
@@ -1,26 +1,62 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+//00000000
+//
+//void swap(int* x, int* y) {
+//	int temp = *x;
+//	*x = *y;
+//	*y = temp;
+//}
+//
+//	void main()
+//{
+//	int x = 10;
+//	int y = 20;
+//
+//	printf("x의 값 : %d\n", x);
+//	printf("y의 값 : %d\n", y);
+//
+//swap(&x, &y);
+//printf("x의 값 : %d\n", x);
+//printf("y의 값 : %d\n", y);	
+//
+//
+//
+//}
+
+
+//void Average(double count, ...)
+//{
+//
+//	va_list point;// 변수명
+//	va_start(point, count); //va_start : 가변 인수를 가져올 수 있도록 포인터를 설정하는 함수
+//								//va_start(포인터, count)
+//	double sum = 0.0; //변수 초기화
+//	for (int i = 0; i < count; i++) 
+//	{
+//		sum += va_arg(point, int);
+//		//va_arg : 가변 인수 포인터에서 특정 자료형의 크기만큼 값을 가져오는 함수
+//		// va_arg(포인터, 특정한 자료형(int 또는 duble)
+//	}
+//	double average = sum / count;
+//
+//	printf("average : %f\n", average);
+//
+//		va_end(point);
+//
+//
+//}
+
+
+//void main()
+//{
+	
+	
+	//Average(5, 92, 90, 88, 95, 100);
 
 
 
 
-void swap(int* x, int* y) {
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
 
-	void main()
-{
-	int x = 10;
-	int y = 20;
-
-	printf("x의 값 : %d\n", x);
-	printf("y의 값 : %d\n", y);
-
-swap(&x, &y);
-printf("x의 값 : %d\n", x);
-printf("y의 값 : %d\n", y);	
-
-
-
-}
+//}
