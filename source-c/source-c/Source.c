1d1d0a4a9d50dd06ff2e86163626d9cc37201923
Ritme54 <tmdwns5456@naver.com>
2025-06-03 15:42:38 +0900

    2025.06.03(메모리)

--- source-c/source-c/Source.c
+++ source-c/source-c/Source.c
@@ -1,200 +1,97 @@
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h> //malloc, free 함수 사용을 위한 헤더 파일
 
 
-void Stats(int health, const char* name)
+int main()
 {
+#pragma region 바이트 비트 단위
+	//바이트는 컴퓨터에서 데이터를 저장하는 기본 단위이며, 1바이트는 8비트로 구성되어 있습니다.
+	//비트는 0 또는 1의 값을 가지며, 바이트는 8개의 비트로 이루어져 있습니다.
+	//1바이트 = 8비트
+#pragma region MyRegion
 
-	printf("health : %d\n", health);
-	printf("name : %s\n", name);
+#pragma endregion
+
+#pragma endregion
+
+	//int a = 10; //a
+
+	//int* ptr = &a; //ptr
+
+#pragma region 동적 할당
+	//프로그램을 실행 중에 필요한 만큼 메모리를 할당하는 작업.
+	
+	//동적 할당은 실행 시간에 가변적으로 메모리의 크키를 변경시킬 수 있으며,
+	//동적으로 메모리의 크기를 할당할 때 바이트 단위로 지정합니다.
+	//동적 할당을 위해서는 stdlib.h 헤더 파일을 포함해야 합니다.
+	//동적 할당을 위해 malloc 함수를 사용하며, 이 함수는 메모리의 크기를 바이트 단위로 지정합니다.
+	//Heap 영역에 메모리를 할당하며, 사용이 끝난 후에는 반드시 free 함수를 사용하여 메모리를 해제해야 합니다.
+	//포인트 타입 void*는 어떤 자료형의 포인터도 가리킬 수 있는 포인터 타입입니다.
+	//오브젝트를 내 마음대로 할당하고 해제할 수 있는 메모리 공간입니다.
+
+	int* pointer = malloc(4); //4바이트 크기의 메모리를 동적으로 할당합니다.
+	
+	*pointer = 10; //할당된 메모리에 값을 저장합니다.
+	
+
+	printf("pointer가 가리키는 값 : %d\n", *pointer);
+
+	free(pointer); //동적으로 할당된 메모리를 해제합니다.
+
+	pointer = malloc(3 * 4); //3개의 int형 변수를 저장할 수 있는 메모리를 동적으로 할당합니다.=12바이트
+	for (int i = 0; i < 3; i++)// for 함수로 연속적으로 메모리를 할당합니다.
+	{
+		pointer[i] = (i + 1) * 10;
+		printf("pointer[%d]의 값 : %d\n", i, pointer[i]);
+	}
+	free(pointer);
+#pragma endregion
+
+#pragma region 추가내용
+
+#pragma region 메모리 파편화
+//메모리 파편화는 동적 할당과 해제를 반복하면서 발생하는 메모리의 비효율적인 사용을 의미합니다.
+//메모리 파편화는 메모리의 할당과 해제를 반복하면서 발생하는 문제로,
+//메모리의 일부가 사용 중이거나 해제되어 있지만, 연속적인 메모리 공간이 부족하여
+//새로운 메모리를 할당할 수 없는 상황을 말합니다.
+//이로 인해 메모리의 효율적인 사용이 저하되고, 프로그램의 성능에 영향을 미칠 수 있습니다.
+
+#pragma endregion
+
+#pragma region 점진적 가비지 컬렉션
+//점진적 가비지 컬렉션은 메모리 관리 기법 중 하나로,
+//프로그램이 실행되는 동안 주기적으로 사용하지 않는 메모리를 자동으로 해제하는 방식입니다.
+//이 기법은 메모리 파편화를 줄이고, 프로그램의 성능을 향상시키는 데 도움을 줍니다.
+#pragma region 점진적 가비지 컬렉션 단점
+	//점진적 가비지 컬렉션의 단점은 메모리 해제 작업이 주기적으로 발생하기 때문에,
+	//프로그램의 성능에 영향을 미칠 수 있다는 점입니다.
+
+#pragma endregion
+
+#pragma endregion
+
+#pragma region 메모리 플릭
+	//메모리 플릭은 메모리 관리 기법 중 하나로,
+	//프로그램이 실행되는 동안 메모리를 효율적으로 관리하기 위해
+	//메모리의 할당과 해제를 반복하는 방식입니다.
+#pragma region 메모리 플릭 단점
+	//메모리 플릭의 단점은 메모리의 할당과 해제를 반복하면서 발생하는 메모리 파편화 문제입니다.
+	//이로 인해 메모리의 효율적인 사용이 저하되고, 프로그램의 성능에 영향을 미칠 수 있습니다.
+#pragma endregion
 
 
+
+#pragma endregion
+
+#pragma region 숙제 uaf
+
+
+
+#pragma endregion
+
+	
+	
+	
+	
+	return 0;
 }
-
-void Swap(int *left, int *right)
-{
-	int temporary = *left;
-	*left = *right;
-	*right = temporary;
-
-
-}
-
-
-inline void collision()
-{
-	
-	printf("You Die");
-
-}
-
-void Provide()
-{
-	printf("Provide\n");
-
-}
-
-void main() 
-{
-
-
-#pragma region 함수
-
-
-	//하나의 특별한 목적의 작업을 수행하기 위해 독립적으로 설계된 코드의 집합.
-	// 
-	//함수는 프로그램이 실행 중일때 함수 호출 위치에서 해당 함수로 이동한 뒤,
-	//작업을 수행하고 다시 원래 위치로 돌아오는 구조
-	// 함수가 작동하면 함수를 찾아가서 위에서 아래로 실행한다.
-	// 
-		//process();
-
-
-
-
-#pragma region 매개변수
-
-//함수의 정의에서 전달받은 인수를 함수 내부로 전달하기 위해 사용하는 변수
-
-//매개 변수는 함수 내부에서만 연산이 이루어지며,
-//여러 개의 매개 변수를 생성할 수 있습니다.
-
-	//Stats(100, "slime");
-
-	//Stats(200, "ime");
-
-	//Stats(300, "lime");
-
-#pragma endregion
-
-
-#pragma region 인수
-	//함수가 호출될 때 매개 변수에 실제로 전달되는 값
-
-	//인수의 경우 값을 전달하는 인수와 값을 전달받는 매개 변수의 자료형이 서로 일치해야 합니다.
-
-	//int X = 10;
-	//int Y = 20;
-	//Swap(&X, &Y);
-	//printf("X의 값 : %p\n", X);
-	//printf("Y의 값 : %p\n", Y);
-	
-
-#pragma endregion
-
-
-
-#pragma endregion
-
-
-#pragma region 레퍼런스
-
-
-#pragma endregion
-
-
-#pragma region Call by Value문제 (숙제)
-	//Call by Refence (참조에 의한 해결)
-	//주소값을 참조해서 해결해보기
-
-
-#pragma endregion
-
-
-#pragma region 재귀 함수
-	//어떤 함수에서 자신을 다시 호출하여 작업을 수행하는 함수입니다.
-		
-	//Recursion(3);
-	
-	//return = 함수를 끝낸다.(종료)
-	
-	//void Recursion(int count)
-//{
-//
-//	if (count <=0)
-//	{
-//		return;
-//	}
-//
-//	printf("Recursion\n");
-//	Recursion(count - 1);
-//}
-
-
-
-#pragma endregion
-
-#pragma region 인라인 함수
-	//함수를 호출하는 대신 함수가 호출되는 위치마다 함수의 코드를 복사하여 전달하는 방식의 함수
-
-	//이벤트 기반 = on이란 전치사 많이 사용
-	// 오버해드(찾아보기)
-	//in line
-
-	
-	//collision();
-
-
-
-	//인라인 함수는 함수를 호출하는 과정이 없으므로 처리 속도가 빠르지만
-	//인라인 함수를 많이 사용하게 되면 함수의 코드가 복사되기 때문에 실행 파일의 크기가 커지게 된다.
-
-
-#pragma endregion
-
-#pragma region 가변 인수 목록
-	//고정되어 있는 매개 변수 외에 개수가 정해지지 않은 인수를 추가로 받을 수 있는 인수입니다.
-
-	//stdarg.h
-	//va_list 인수 목록
-	//va_start 시작 포인터 주소 가르키기
-	//->va_arg 값을 반환하는 함수
-	//va_end (안해도 큰 문제가 없을 수 있음) NULL로 초기화를 시켜주는 변수
-	
-	//void average(int count, ...)
-	// 
-	//va_list : 가변 인수의 메모리 주소를 저장하는 포인터 변수
-	//va_list : 변수
-	//va_start : 가변 인수를 가져올 수 있도록 포인터를 설정하는 함수
-	//va_start(포인터, count)
-	// va_arg : 가변 인수 포인터에서 특정 자료형의 크기만큼 값을 가져오는 함수
-	// va_arg(포인터, 특정한 자료형(int 또는 duble)
-	// va_end : 가변 인수가 끝났을 때 포인터를 NULL로 초기화하는 함수
-	// va_end(포인터)	
-
-
-#pragma endregion
-
-#pragma region 함수 포인터
-	//함수의 주소 값을 저장하고 가리킬 수 있는 포인터 변수입니다.
-	//자료형(*변수 이름)(매개변수, 매개변수)
-	//대신 호출하는 방식
-
-
-
-
-//포인터가 시작 주소를 가리킨다.
-
-//void Provide()
-//{
-//	printf("Probide\n")
-//
-//}
-
-//서로 커플링 시키지 않고 이벤트 버스를 만들어서 대신 호출하게 한다.
-
-void(*functionPointer)();
-
-functionPointer = Provide;
-
-functionPointer();
-
-	
-#pragma endregion
-
-
-
-
-}
-
-
--- source-c/구구단 숙제/레퍼.c
+++ source-c/구구단 숙제/레퍼.c
@@ -50,13 +50,37 @@
 
 
 //void main()
-//{
+{
+#pragma region 가변인수목록
+	////가변 인수 목록은 고정된 매개 변수 외에 개수가 정해지지 않은 인수를 추가로 받을 수 있는 인수입니다.
+	//stdarg.h 헤더 파일을 포함하여 가변 인수 목록을 사용할 수 있습니다.
+	////void Average(int count, ...)
+	//{100, 50, 30, 200, 150, 80, 90, 70, 60, 40, 20, 10};
+	//va_list : 가변 인수의 메모리 주소를 저장하는 포인터 변수
+	//va_list point; //변수
+	//va_start : 가변 인수를 가져올 수 있도록 포인터를 설정하는 함수
+	//va_start(point, count);
+	//double sum = 0.0; //변수 초기화
+	//for (int i = 0; i < count; i++)
+	//{
+	//	sum += va_arg(point, int);
+	//	//va_arg : 가변 인수 포인터에서 특정 자료형의 크기만큼 값을 가져오는 함수
+	//	// va_arg(포인터, 특정한 자료형(int 또는 duble)
+	//}
+	//double average = sum / count;
+	//printf("average : %f\n", average);
+	//va_end(point);
+
+#pragma endregion
+#pragma region 가변인수목록으로 평균구하기
+		//가변 인수 목록을 사용하여 평균을 구하는 함수입니다.
+	//int count = 12;
+	//Average(count, 100, 50, 30, 200, 150, 80, 90, 70, 60, 40, 20, 10);
+
 	
+
+#pragma endregion
+
+
 	
-	//Average(5, 92, 90, 88, 95, 100);
-
-
-
-
-
-//}
+}
